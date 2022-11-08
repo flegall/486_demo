a65c396daaade85fdc792e9fed4aea0e1dd83d43
Florent Le Gall <florent.legall@gmail.com>
2022-11-08 10:38:59 +0100

    Add test.txt and remove reading autoexec.bat

--- src/dos_tests/file.rs
+++ src/dos_tests/file.rs
@@ -12,16 +12,5 @@ pub(crate) fn file_read_test() {
         Err(_) => println!("Error closing file")
     }
 
-    let test_file = dos::file::File::open("C:\\AUTOEXEC.BAT").unwrap();
-    let mut buffer = [0; 100];
-    let bytes_read = test_file.read(&mut buffer).unwrap();
-    println!("{} bytes read", bytes_read);
-    println!("{}", core::str::from_utf8(&buffer).unwrap());
-
-    match test_file.close() {
-        Ok(_) => println!("File closed"),
-        Err(_) => println!("Error closing file")
-    }
-
     println!("Hello, World!");
 }
